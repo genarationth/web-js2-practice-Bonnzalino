43878f2a9d49412365a0677c2321461cce248cb8
Bonnzalino <siwakorn.paorod@gmail.com>
2023-03-11 13:57:07 +0700

    Finished Part2

---
+++ script.js
@@ -0,0 +1,29 @@
+// Part 2 Challenge 1
+// Create an Age Calculator
+{
+let birthYear = 1994;
+let futureYear = 2028;
+
+let diffYear = futureYear - birthYear;
+
+//Show in console
+
+console.log(`I was born in ${birthYear} , then in ${futureYear} I,ll be ${diffYear}.`);
+
+}
+
+
+// Part 2 Challenge 2
+// Create an Information card
+{    
+const info = {
+    name: 'Bonn',
+    location: 'Saraburi',
+    hobby: 'sleeping'
+}
+
+//Show in console
+
+console.log(`Hi, my name is ${info.name}. I live in ${info.location} and enjoy ${info.hobby}.`);
+
+}
